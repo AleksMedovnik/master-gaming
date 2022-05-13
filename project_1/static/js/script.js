d3d446d7c3a017d497bf618816b43a4a24733863
Aleksandr <medovnikaleksander@yandex.ua>
2022-05-13 12:40:54 +0300

    Added static/js/script.js

---
+++ project_1/static/js/script.js
@@ -0,0 +1,12 @@
+        function showActiveLink(menu) {
+            try {
+                let elem = document.getElementById(menu).querySelectorAll('a');
+                let url = document.location.href;
+                for (const link of elem) {
+                    if (url === link.href) {
+                        link.classList.add('active');
+                    };
+                };
+            } catch (e) { }
+        };
+        showActiveLink('menu')
