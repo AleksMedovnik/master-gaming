4464e44ef1bddb43a892ac24126e7a7e2c8cbe8a
Aleksandr <medovnikaleksander@yandex.ua>
2022-07-01 18:02:17 +0300

    Finished Blog

--- project_1/blog/admin.py
+++ project_1/blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Post, Tag
+from .models import Post, Tag, Comment
 
 admin.site.register(Post)
 admin.site.register(Tag)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+admin.site.register(Comment, CommentAdmin)
