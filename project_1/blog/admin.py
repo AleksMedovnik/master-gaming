c8606c21097b2d3b91fbc3ad43cb381b18c90fdc
Aleksandr <medovnikaleksander@yandex.ua>
2022-07-16 12:53:31 +0300

    delete comments

--- project_1/blog/admin.py
+++ project_1/blog/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from .models import Post, Tag, Comment, Product
+from .models import Post, Tag, Product
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Product)
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_fields = ('name', 'email', 'body')
-admin.site.register(Comment, CommentAdmin)
